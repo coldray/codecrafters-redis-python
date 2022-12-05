2cc3a8befb70ab4a9c9956a8587c9e9ff11eb7c3
Raymond Zou <raymondjoe16@gmail.com>
2022-12-04 20:18:55 -0800

    respond to multiple pings

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_connection, _ = server_socket.accept()  # wait for client
 
     while True:
+    	client_connection, _ = server_socket.accept()  # wait for client
     	client_connection.recv(1024)  # wait for client to send data
 	    client_connection.send(b"+PONG\r\n")
 
