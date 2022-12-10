16e7632629564183d27cb9841592fc5b8f306294
Raymond Zou <raymondjoe16@gmail.com>
2022-12-10 15:09:52 -0800

    implement echo

--- app/main.py
+++ app/main.py
@@ -1,23 +1,31 @@
-import socket
+
 import threading
+import socket
+from .resp_decoder import RESPDecoder
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        client_connection, _ = server_socket.accept()  # wait for client
-        thread = threading.Thread(target=handle_connection, args=(client_connection,))
-        thread.start()
-    
 def handle_connection(client_connection):
     while True:
         try:
-            client_connection.recv(1024) # wait for client to send data
-            client_connection.send(b"+PONG\r\n")
+           command, *args = RESPDecoder(client_connection).decode()
+
+           if command == b"ping":
+               client_connection.send(b"+PONG\r\n")
+           elif command == b"echo":
+               client_connection.send(b"$%d\r\n%b\r\n" % (len(args[0]), args[0]))
+           else:
+               client_connection.send(b"-ERR unknown command\r\n")
         except ConnectionError:
-            break
-	
+            break  # Stop serving if the client connection is closed
+
+
+def main():
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+
+    while True:
+        client_connection, _ = server_socket.accept()  # wait for client
+        threading.Thread(target=handle_connection, args=(client_connection,)).start()
+
+
 if __name__ == "__main__":
     main()
